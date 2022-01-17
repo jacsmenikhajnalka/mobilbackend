7ea650e907cd51628a0b16fa3a7acfffe87ab7bd
tanulo <tanulo@barossgabor.local>
2022-01-17 11:32:25 +0100

    teszt

--- backend.js
+++ backend.js
@@ -4,6 +4,7 @@ const app = express()
 const port = 3000
 
 app.use(express.static('kepek'))
+app.use(express.static('konyv_kep'))
 
 app.use(cors())
 
@@ -14,7 +15,7 @@ app.get('/', (req, res) => {
 })
 
 
-app.get('/konyv', (req, res) => {
+app.get('/konyv_fajtai', (req, res) => {
 
     var mysql = require('mysql')
     var connection = mysql.createConnection({
@@ -26,7 +27,7 @@ app.get('/konyv', (req, res) => {
     
     connection.connect()
     
-    connection.query('SELECT * from konyv', function (err, rows, fields) {
+    connection.query('SELECT * from konyv_fajtai', function (err, rows, fields) {
       if (err) throw err
     
       console.log(rows)
@@ -38,7 +39,56 @@ app.get('/konyv', (req, res) => {
 
   
   })
+  app.get('/konyv', (req, res) => {
 
+    var mysql = require('mysql')
+    var connection = mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: '',
+      database: 'konyv_adatb'
+    })
+    
+    connection.connect()
+    
+    connection.query('SELECT `leiras_konyv`, konyv.konyv_kep, konyv.konyv_nev FROM `leiras` INNER JOIN konyv_fajtai ON leiras_id=konyv_fajtai.konyv_id INNER JOIN konyv ON konyv_fajtai.konyv_id = konyv.konyv_id', function (err, rows, fields) {
+      if (err) throw err
+    
+      console.log(rows)
+      res.send(rows)
+    })
+    
+    connection.end()
+
+
+  
+  }) 
+
+
+  app.get('/konyv_fajtai', (req, res) => {
+
+    var mysql = require('mysql')
+    var connection = mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: '',
+      database: 'konyv_adatb'
+    })
+    
+    connection.connect()
+    
+    connection.query('SELECT `konyv_nev`, konyv_fajtai.konyvfajta_kep FROM `konyv` INNER JOIN konyv_fajtai ON konyv.konyv_id=konyv_fajtai.konyv_id', function (err, rows, fields) {
+      if (err) throw err
+    
+      console.log(rows)
+      res.send(rows)
+    })
+    
+    connection.end()
+
+
+  
+  }) 
 
   app.post('/szavazatfelvitel', (req, res) => {
 
@@ -47,7 +97,7 @@ app.get('/konyv', (req, res) => {
       host: 'localhost',
       user: 'root',
       password: '',
-      database: 'kony_tipus'
+      database: 'konyv_adatb'
     })
     
     connection.connect()
@@ -55,8 +105,8 @@ app.get('/konyv', (req, res) => {
     connection.query('insert into szavazat values (null, '+req.body.bevitel1+' )', function (err, rows, fields) {
       if (err) throw err
     
-      console.log("Szavazatát rögzítettük!")
-      res.send("Szavazatát rögzítettük!")
+      console.log(rows)
+      res.send(rows)
     })
     
     connection.end()
